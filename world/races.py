cf0c7a8e88183136ab5e0e187c06384e37b90c21
roymcsp <roy.mcsp@gmail.com>
2017-04-15 13:48:43 -0700

    races.py
    all races added

--- world/races.py
+++ world/races.py
@@ -15,7 +15,8 @@ class Race(object):
             'wrists': None,
             'hands': None,
             'finger1': None,
-            'finger2': None,        
+            'finger2': None,
+            'feet': None,            
         }
         
         self.limbs = (
@@ -29,11 +30,13 @@ class Race(object):
             ('hands', ('hands',)),
             ('finger1', ('finger1',)),
             ('finger2', ('finger2',)),
+            ('feet' , ('feet',)),
         )
         
         self.bonuses = {}
         self.language = {}
-        
+
+       
 class Human(Race):
     """Class representing human attributes."""
     def __init__(self):
@@ -42,15 +45,174 @@ class Human(Race):
         self.plural = "Humans"
         self.size = "medium"
         self.language = "common"
-        
+#Kingdom Races         
 class Elf(Race):
     """Class reresenting elven attributes."""
     def __init__(self):
-        super(Human, self).__init__()
+        super(Elf, self).__init__()
         self.name = "Elf"
         self.plural = "Elves"
         self.size = "medium"
         self.bonuses = {'Dexterity': 2,
                         'Constitution': -2}
         self.language = "Elven"
+        
+class Dwarf(Race): 
+    """Class reresenting dwarven attributes."""
+    def __init__(self):
+        super(Dwarf, self).__init__()
+        self.name = "Dwarf"
+        self.plural = "Dwarfs"
+        self.size = "medium"
+        self.bonuses = {'Constitution': 2,
+                        'Charisma': -2}
+        self.language = "Dwarven"
+        
+class Gnome(Race): 
+    """Class reresenting gnomish attributes."""
+    def __init__(self):
+        super(Gnome, self).__init__()
+        self.name = "Gnome"
+        self.plural = "Gnomes"
+        self.size = "small"
+        self.bonuses = {'Constitution': 2,
+                        'Strength': -2}
+        self.language = "Gnomish"
+
+class Centaur(Race): 
+    """Class reresenting centaur attributes."""
+    def __init__(self):
+        super(Centaur, self).__init__()
+        self.name = "Centaur"
+        self.plural = "Centaur"
+        self.size = "large"
+        self.bonuses = {'Wisdom': 2,
+                        'Intelligence': -2}
+        self.language = "Centaur" 
+
+class Ogryn(Race): 
+    """Class reresenting Ogryn attributes."""
+    def __init__(self):
+        super(Dwarf, self).__init__()
+        self.name = "Ogryn"
+        self.plural = "Ogryn"
+        self.size = "medium"
+        self.bonuses = {'Strength': 2,
+                        'Intelligence': -2}
+        self.language = "Ogryn"
+          
+
+#Calipahte Races
+Dark Elf 
+class Drow(Race): 
+    """Class reresenting Drow attributes."""
+    def __init__(self):
+        super(Drow, self).__init__()
+        self.name = "Drow"
+        self.plural = "Drow"
+        self.size = "medium"
+        self.bonuses = {'': 2,
+                        '': -2}
+        self.language = "Drow"
+
+class Duergar(Race): 
+    """Class reresenting elven attributes."""
+    def __init__(self):
+        super(Duergar, self).__init__()
+        self.name = "Duergar"
+        self.plural = "Duergar"
+        self.size = "medium"
+        self.bonuses = {'': 2,
+                        '': -2}
+        self.language = "Duergar"        
+
+class Svirfneblin(Race): 
+    """Class reresenting Svirfneblin attributes."""
+    def __init__(self):
+        super(Svirfneblin, self).__init__()
+        self.name = "Svirfneblin"
+        self.plural = "Svirfneblin"
+        self.size = "small"
+        self.bonuses = {'': 2,
+                        '': -2}
+        self.language = "Svirfneblin"
+Deep gnome 
+class Wemic(Race): 
+    """Class reresenting Wemic attributes."""
+    def __init__(self):
+        super(Wemic, self).__init__()
+        self.name = "Wemic"
+        self.plural = "Wemic"
+        self.size = "large"
+        self.bonuses = {'': 2,
+                        '': -2}
+        self.language = "Wemic"
+
+class Drakkar(Race): 
+    """Class reresenting Drakkari attributes."""
+    def __init__(self):
+        super(Drakkar, self).__init__()
+        self.name = "Drakkar"
+        self.plural = "Drakkari"
+        self.size = "medium"
+        self.bonuses = {'': 2,
+                        '': -2}
+        self.language = "Drakkonic"
+
+
+#Empire Races 
+class Ursine(Race): 
+    """Class reresenting elven attributes."""
+    def __init__(self):
+        super(Dwarf, self).__init__()
+        self.name = "Ursine"
+        self.plural = "Ursine"
+        self.size = "large"
+        self.bonuses = {'': 2,
+                        '': -2}
+        self.language = "Ursine"
+ 
+class Lupine(Race): 
+    """Class reresenting lupine attributes."""
+    def __init__(self):
+        super(Lupine, self).__init__()
+        self.name = "Lupine"
+        self.plural = "Lupine"
+        self.size = "medium"
+        self.bonuses = {'': 2,
+                        '': -2}
+        self.language = "Lupine"
+
+class Feline(Race): 
+    """Class reresenting feline attributes."""
+    def __init__(self):
+        super(Feline, self).__init__()
+        self.name = "Feline"
+        self.plural = "Feline"
+        self.size = "medium"
+        self.bonuses = {'': 2,
+                        '': -2}
+        self.language = "Feline"
+ 
+class Vulpine(Race): 
+    """Class reresenting vulpine attributes."""
+    def __init__(self):
+        super(Vulpine, self).__init__()
+        self.name = "Vulpine"
+        self.plural = "Vulpine"
+        self.size = "small"
+        self.bonuses = {'': 2,
+                        '': -2}
+        self.language = "Vulpine"
+
+class Naga(Race): 
+    """Class reresenting nagan attributes."""
+    def __init__(self):
+        super(Naga, self).__init__()
+        self.name = "Naga"
+        self.plural = "Naga"
+        self.size = "medium"
+        self.bonuses = {'': 2,
+                        '': -2}
+        self.language = "Nagan"
         
