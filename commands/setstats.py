d8e50be27693129638ee44b1a374a5bc17ed5332
BlauFeuer <phaselinear700series2@gmail.com>
2017-04-15 14:25:41 -0400

    Fold long lines
    How to fold long lines of string text in code while still keeping it as if it was long line for output.

--- commands/setstats.py
+++ commands/setstats.py
@@ -9,7 +9,13 @@ class CmdSetStats(default_cmds.MuxCommand):
     def func(self):
         caller = self.caller
         rolls = 25
-        msg1 = "Runaria uses a method of rolling the dice to create player attributes. These attributes are: strength, dexterity, constitution, intelligence, wisdom, and charisma. Choose the order of your attributes from the most important to your character to the least important.\n\nAn example is as follows: strength dexterity constitution intelligence wisdom charisma\n\nYou will only be able to roll 25 times, so when you see a decent roll you should accept it."
+        msg1 = ("Runaria uses a method of rolling the dice to create player attributes. "
+                "These attributes are: strength, dexterity, constitution, intelligence, "
+                "wisdom, and charisma. Choose the order of your attributes from the most "
+                "important to your character to the least important.\n\nAn example is as "
+                "follows: strength dexterity constitution intelligence wisdom charisma\n\n"
+                "You will only be able to roll 25 times, so when you see a decent roll, "
+                "you should accept it.")
         if self.caller.db.stat_rolled:            
             return self.caller.msg('You may only roll your stats once.')
         elif not self.caller.db.stat_rolled:
