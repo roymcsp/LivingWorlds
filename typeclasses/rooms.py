7343f1794b23fff90e8a45a3c6a77fc852f4a4c9
roymcsp <roy.mcsp@gmail.com>
2017-11-14 07:32:17 -0800

    fix to ExtendedTime

--- typeclasses/rooms.py
+++ typeclasses/rooms.py
@@ -78,6 +78,7 @@ from evennia import default_cmds
 from evennia import utils
 from evennia.contrib.rpsystem import ContribRPRoom
 from evennia.utils.spawner import spawn
+from evennia.contrib import custom_gametime
 
 # error return function, needed by Extended Look command
 _AT_SEARCH_RESULT = utils.variable_from_module(*settings.SEARCH_AT_RESULT.rsplit('.', 1))
@@ -527,10 +528,13 @@ class CmdGameTime(default_cmds.MuxCommand):
             prep = "a"
             if season == "autumn":
                 prep = "an"
-            year, month, day, hour, min, = gametime.gametime(absolute=True)
-            self.caller.msg("It's %s %s day, in the %s. The time is %s:%s on %s, %s, %s" % (prep, season, timeslot,
-                                                                                            {hour}, {min}, {month},
-                                                                                            {day}, {year}))
+            self.caller.msg("It's %s %s day, in the %s." % (prep, season, timeslot,))
+
+        year, month, day, hour, min, sec = custom_gametime.custom_gametime(absolute=True)
+        string = "It's {hour:02}:{min:02}. {month}/{day]/{year]."
+        self.msg(string.format(year=year, month=month, day=day,
+                                   hour=hour, min=min, sec=sec))
+
 
 class ChargenRoom(Room):
     """
