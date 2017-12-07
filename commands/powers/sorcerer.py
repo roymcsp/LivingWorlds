2abebb5669fbe8cad359fb21044e44c163fe5a7e
roymcsp <roy.mcsp@gmail.com>
2017-12-07 15:12:49 -0800

    fix to sorc powers

--- commands/powers/sorcerer.py
+++ commands/powers/sorcerer.py
@@ -165,7 +165,7 @@ class CmdCursedBone(MuxCommand):
             "|M{actor} finishes chanting and produces some cursed bones.|n",
             mapping=dict(actor=caller),
             exclude=caller)
-        create_object(typeclass='typeclasses.sorcobjects.CursedBone', location=caller.contents)
+        create_object(typeclass='typeclasses.sorcobjects.CursedBone', location=caller)
         # if the spell was successfully cast, store the casting time
         caller.db.cursedbone_lastcast = time.time()
 
