6d740a0c3177c1adf111b93da36f5fbd509b6b90
roymcsp <roy.mcsp@gmail.com>
2017-12-09 10:14:13 -0800

    changes to sorcobjects added an object

--- typeclasses/sorcobjects.py
+++ typeclasses/sorcobjects.py
@@ -1,18 +1,37 @@
-from objects import Object
+from items import Item
 
-class CursedBone(Object):
+
+class CursedBone(Item):
 
     def at_object_creation(self):
+        super(CursedBone, self).at_object_creation()
         self.key = "Cursed bone"
-        self.name = "{xCursed bone{n"
-        self.aliases = "Cursed bone"
+        self.name = "|xCursed bone|n"
+        self.aliases = ["Cursed bone","bone"]
         self.db.desc = "a fragment of bone that has been imbued with the energies of the netherworld"
         self.db.weight = 0.1
-    
-class BloodGem(Object):
+
+
+class BloodGem(Item):
+
     def at_object_creation(self):
-        key = "Blood Gem"
-        name = "{RBlood gem{n"
+        super(BloodGem, self).at_object_creation()
+        self.key = "Blood Gem"
+        self.name = "|RBlood gem|n"
         self.db.desc = "Glowing with inner light, this gem is forged from the crystallized blood of a sorcerer and imbued with restorative powers"
         self.db.weight = 0.05
-        #self.cmdset.add_default(CmdSetConsumable, permanent=True)
+
+    def at_consume(self):
+        pass
+
+
+class BoneDust(Item):
+    def at_object_creation(self):
+        super(BoneDust,self).at_object_creation()
+        self.key = ""
+        self.name = ""
+        self.db.desc = ""
+        self.db.weight = ""
+
+    def at_use(self):
+        pass
