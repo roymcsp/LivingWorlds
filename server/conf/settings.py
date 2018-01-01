9fa4915f42467b32fe12d9ec0eccce8fc770fbb2
roymcsp <roy.mcsp@gmail.com>
2018-01-01 12:03:57 -0800

    change TZ

--- server/conf/settings.py
+++ server/conf/settings.py
@@ -66,7 +66,7 @@ except ImportError:
 
 # Local time zone for this installation. All choices can be found here:
 # http://www.postgresql.org/docs/8.0/interactive/datetime-keywords.html#DATETIME-TIMEZONE-SET-TABLE
-TIME_ZONE = 'America/New_York'
+TIME_ZONE = 'America/Los_Angeles'
 TIME_FACTOR = 1.0
 TIME_GAME_EPOCH = None
 
