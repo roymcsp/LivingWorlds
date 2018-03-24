2b5fac999f52516b9071fd5c440dc6da3f5bd39f
roymcsp <roy.mcsp@gmail.com>
2018-03-24 08:18:23 -0700

    adding wiki

--- web/urls.py
+++ web/urls.py
@@ -5,13 +5,15 @@ help.
 
 """
 from django.conf.urls import url, include
-
+from django_nyt.urls import get_pattern as get_nyt_pattern
+from wiki.urls import get_pattern as get_wiki_pattern
 # default evennia patterns
 from evennia.web.urls import urlpatterns
 
 # eventual custom patterns
 custom_patterns = [
-    # url(r'/desired/url/', view, name='example'),
+    url(r'^notifications/', get_nyt_pattern()),
+    url(r'^wiki/', get_wiki_pattern())
 ]
 
 # this is required by Django.
